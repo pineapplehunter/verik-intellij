f8ea58bec963673b7ce1d6fce4c3ed40903104dc
GitHub Action <action@github.com>
2021-12-23 21:20:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/frwang96/verikintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/frwang96/verikintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.frwang96.verikintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.frwang96.verikintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
