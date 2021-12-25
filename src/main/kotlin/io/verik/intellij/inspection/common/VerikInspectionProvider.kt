4c210c2a99f936b686d3dbd9f754cabafa2c8c81
Francis Wang <frwang@mit.edu>
2021-12-25 16:13:14 +0800

    Unsupported modifier inspection

--- src/main/kotlin/io/verik/intellij/inspection/common/VerikInspectionProvider.kt
+++ src/main/kotlin/io/verik/intellij/inspection/common/VerikInspectionProvider.kt
@@ -19,10 +19,14 @@ package io.verik.intellij.inspection.common
 import com.intellij.codeInspection.InspectionToolProvider
 import com.intellij.codeInspection.LocalInspectionTool
 import io.verik.intellij.inspection.inspection.UnsupportedElementInspection
+import io.verik.intellij.inspection.inspection.UnsupportedModifierInspection
 
 class VerikInspectionProvider : InspectionToolProvider {
 
     override fun getInspectionClasses(): Array<Class<out LocalInspectionTool>> {
-        return arrayOf(UnsupportedElementInspection::class.java)
+        return arrayOf(
+            UnsupportedElementInspection::class.java,
+            UnsupportedModifierInspection::class.java
+        )
     }
 }
---
+++ src/main/kotlin/io/verik/intellij/inspection/inspection/UnsupportedModifierInspection.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021 Francis Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.verik.intellij.inspection.inspection
+
+import com.intellij.codeHighlighting.HighlightDisplayLevel
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import io.verik.intellij.inspection.common.AbstractVerikInspection
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.modifierListVisitor
+
+class UnsupportedModifierInspection : AbstractVerikInspection() {
+
+    private val unsupportedModifiers = listOf(
+        KtTokens.ANNOTATION_KEYWORD,
+        KtTokens.CROSSINLINE_KEYWORD,
+        KtTokens.DATA_KEYWORD,
+        KtTokens.EXTERNAL_KEYWORD,
+        KtTokens.IN_KEYWORD,
+        KtTokens.NOINLINE_KEYWORD,
+        KtTokens.OPERATOR_KEYWORD,
+        KtTokens.OUT_KEYWORD,
+        KtTokens.REIFIED_KEYWORD,
+        KtTokens.SEALED_KEYWORD,
+        KtTokens.SUSPEND_KEYWORD,
+        KtTokens.TAILREC_KEYWORD,
+        KtTokens.VARARG_KEYWORD
+    )
+
+    override fun getID(): String {
+        return "VerikModifierException"
+    }
+
+    override fun getDisplayName(): String {
+        return "Unsupported modifier"
+    }
+
+    override fun getStaticDescription(): String {
+        return "Reports modifiers that are not supported by Verik"
+    }
+
+    override fun getDefaultLevel(): HighlightDisplayLevel {
+        return HighlightDisplayLevel.ERROR
+    }
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return modifierListVisitor { modifierList ->
+            unsupportedModifiers.forEach {
+                if (modifierList.hasModifier(it)) {
+                    val modifier = modifierList.getModifier(it)!!
+                    holder.registerProblem(modifier, "Modifier ${modifier.text} not supported")
+                }
+            }
+        }
+    }
+}
